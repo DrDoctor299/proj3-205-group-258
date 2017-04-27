3330afc4bfd92f50d0c646ed69d7e476a474f325
sllopis <sllopis@csumb.edu>
2017-04-27 11:18:05 -0700

    Added program for Google Directions

--- sergio.py
+++ sergio.py
@@ -1,86 +1,54 @@
-import discord
-from discord.ext import commands
-import random
+#Google Directions API in Python
+#Comments Added by Sergio.
 
-description = '''/add , /multiply , /divide , /subtract , /roll , /choose , /repeat ,
-/joined
-                 .'''
-bot = commands.Bot(command_prefix='/', description=description)
+import json, urllib
+from urllib import urlencode
+import googlemaps
 
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+# Asking the User for Input for Start and Final Destination.
+userStart = raw_input("Please enter your start destination:  ")
+userFinish = raw_input("Please enter your final destination:  ")
 
-# Bot Adding
+# Opening the API website using json and encoding it.
+url = 'http://maps.googleapis.com/maps/api/directions/json?%s' % urlencode((
+           #Parameters for beginning and final destinations.
+            ('origin', userStart),
+            ('destination', userFinish)
+ ))
+ 
+#Using the URLlib module to open up the URL: Maps.Googleapis.com.
+ur = urllib.urlopen(url)
 
-@bot.command()
-async def add(left : int, right : int):
-    """Adds two numbers together."""
-    await bot.say(left + right)
+#Parsing and decoding the URL.
+#Using JavaScript Object Notation (json).
+result = json.load(ur)
+
+#Function to Remove HTML tags
+def remove_html(text):
+    tag = False   # < >
+    quote = False  # " '
+    clean_text = ""  # Plain string
+
+#Itirating through the text to identify special symbols "</'>
+    for current in text:
+            if current == '<' and not quote:
+                tag = True
+            elif current == '>' and not quote:
+                tag = False
+            elif (current == '"' or current == "'") and tag:
+                quote = not quote
+            elif not tag:
+                clean_text = clean_text + current
+#Returning the actual plain text
+    return clean_text
     
-# Bot Subtracting
-@bot.command() 
-async def subtract(left : int, right : int):
-    """Subtracts two numbers together."""
-    await bot.say(left - right)
+
+#Printing the already decoded URL.
+for i in range (0, len (result['routes'][0]['legs'][0]['steps'])):
     
-@bot.command() 
-async def divide(left : int, right : int):
-    """Divides two numbers together."""
-    await bot.say(float (left / right))
+    #Repeated Routes, legs, steps printed in HTML format.
+    j = result['routes'][0]['legs'][0]['steps'][i]['html_instructions']
+    #Calling the function remove_html to remove HTML TAGS & QUOTES.
+    print remove_html(j)
+
     
-@bot.command() 
-async def multiply(left : int, right : int):
-    """Multiplies two numbers together."""
-    await bot.say(left * right)
-
-@bot.command()
-async def roll(dice : str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await bot.say('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await bot.say(result)
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(*choices : str):
-    """Chooses between multiple choices."""
-    await bot.say(random.choice(choices))
-
-@bot.command()
-async def echo(*, message: str):
-    """Echoes a message once"""
-    await bot.say(message)
-
-@bot.command()
-async def repeat(times : int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await bot.say(content)
-
-@bot.command()
-async def joined(member : discord.Member):
-    """Says when a member joined."""
-    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
-
-@bot.group(pass_context=True)
-async def cool(ctx):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
-
-@cool.command(name='bot')
-async def _bot():
-    """Is the bot cool?"""
-    await bot.say('Yes, the bot is cool.')
-
-bot.run('token')
